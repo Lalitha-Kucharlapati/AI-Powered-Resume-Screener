48f9a88c0eb19df9799a717520d384a690184425
Lalitha-Kucharlapati <lalithakucharlapati0@gmail.com>
2025-04-03 16:48:55 +0530

    Deleted filename

--- a.py
+++
@@ -1,7 +0,0 @@
-import os
-
-font_path = "fonts/DejaVuSans.ttf"  # Adjust if needed
-if not os.path.exists(font_path):
-    print(f"Font file not found: {font_path}")
-else:
-    print("Font file exists.")
